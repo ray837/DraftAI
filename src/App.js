a365ff285727dc56fc84e871789a00fc23af834b
ray837 <59028643+ray837@users.noreply.github.com>
2025-05-12 12:31:25 +0530

    config internal API

--- src/App.js
+++ src/App.js
@@ -149,7 +149,7 @@ function App() {
        
       const response = await axios.post(
       //  'https://openrouter.ai/api/v1/chat/completions',
-       'https://llm-api.iservebetter.idfcbank.com/qwen3-14b/v1/completions',
+       'https://llm-api.iservebetter.idfcfirstbank.com/qwen3-14b/v1/completions',
         {
           // model: 'openai/gpt-3.5-turbo',
           // messages: [{ role: 'user', content: prompt }],
