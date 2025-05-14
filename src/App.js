35cb9c41e481a588c95d9ed23c02d275c45240a7
ray837 <59028643+ray837@users.noreply.github.com>
2025-05-14 21:29:07 +0530

    api config

--- src/App.js
+++ src/App.js
@@ -148,20 +148,20 @@ function App() {
       }
        
       const response = await axios.post(
-      //  'https://openrouter.ai/api/v1/chat/completions',
-       'https://llm-api.iservebetter.idfcfirstbank.com/qwen3-14b/v1/completions',
+       'https://openrouter.ai/api/v1/chat/completions',
+      //  'https://llm-api.iservebetter.idfcfirstbank.com/qwen3-14b/v1/completions',
         {
-          // model: 'openai/gpt-3.5-turbo',
-          // messages: [{ role: 'user', content: prompt }],
-          // temperature: 0.7,
-          model:'/app/models/Qwen3-14B-FP8',
-          prompt:prompt
+          model: 'openai/gpt-3.5-turbo',
+          messages: [{ role: 'user', content: prompt }],
+          temperature: 0.7,
+          // model:'/app/models/Qwen3-14B-FP8',
+          // prompt:prompt
         },
         {
           headers: {
            
-            // Authorization: `Bearer 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`,
-            Authorization: `Bearer cmF5aWQuYWhtZWQ6QXBleEAxMjM0`,
+            Authorization: `Bearer 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`,
+            // Authorization: `Bearer cmF5aWQuYWhtZWQ6QXBleEAxMjM0`,
           
             'Content-Type': 'application/json',
             // 'Access-Control-Allow-Origin':'https://ray837.github.io'
